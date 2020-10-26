fa301d6ded5e01fb2111e316829dd39e3a0c898c
jrkozak <jrkozak92@gmail.com>
2020-10-26 14:36:57 -0500

    initial dist subtree commit

---
+++ dist/js/chunk-2d0bff10.d984e0eb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bff10"],{4053:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"component"},[e._v(" Database interaction will go here ")])},s=[],c={name:"database"},l=c,i=t("2877"),o=Object(i["a"])(l,a,s,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d0bff10.d984e0eb.js.map
