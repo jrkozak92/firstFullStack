c485eaedf26d9d9bbb9078bda611db8fc50b392b
jrkozak <jrkozak92@gmail.com>
2020-12-01 14:56:49 -0600

    database tab updates

--- src/components/database.vue
+++ src/components/database.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="component">
     <div class="search">
+      <button 
+      class="refreshButton"
+      type="button"
+      @click="refreshList()">
+        Refresh Project List
+      </button>
       <input 
         type="text"
         class="search-title" 
@@ -20,29 +26,32 @@
       <li class="project-card"
         :class="{ active: index == currentIndex }"
         v-for="(project, index) in projects" 
-        :key="index"
-        @click="setActiveProject(project, index)">
-        <p class="proj-title">
-          {{ project.title }}
+        :key="index">
+        <p class="proj-title" @click="setActiveProject(project, index)">
+          Title: {{ project.title }}
         </p>
         <p class="proj-desc">
-          {{ project.description }}
+          Description: {{ project.description }}
         </p>
-        <p class="proj-link">
-          {{ project.link }}
+        <p class="proj-link" v-show="project.link">
+          Link: {{ project.link }}
         </p>
-        <input v-model="pass" v-on:keyup="checkPass" placeholder="Enter the secret password...">
-        <div>{{ pass }}</div>
-        <p v-show="showButtons" v-if="pass == 'duh'"> {{ showMessage }}
-          <button @click="saveToDB(project, index)">
-            Save Changes
-          </button>
-          <button @click="deleteFromDB(project, index)">
-            Delete this Project
-          </button>
-        </p>
-        <p v-show="!showbuttons && pass === 'secrect password'"> {{ showMessage }} </p>
-        <p v-show="!showButtons"> {{ showMessage }} </p>
+        <div class="editBox" v-show="showEdit">
+          <input v-model="pass" v-on:keyup="checkPass" placeholder="Enter the secret password...">
+          <p>Your Guess: {{ pass }}</p>
+          <p v-show="showButtons" v-if="pass == 'duh'"> {{ showMessage }}
+            <button @click="saveToDB(project, index)">
+              Save Changes
+            </button>
+            <button @click="deleteFromDB(project, index)">
+              Delete this Project
+            </button>
+          </p>
+          <p v-show="!showButtons && pass === 'the secrect password'">Response: {{ showMessage }} </p>
+          <p v-show="!showButtons && pass === 'secrect password'">Response: {{ showMessage }} </p>
+          <p v-show="!showButtons">Response: {{ showMessage }} </p>
+        </div>
+        <button @click="toggleShowEdit()">{{ editMessage }}</button>
       </li>
     </ul>
   </div>
@@ -58,7 +67,10 @@ export default {
       projects: [{ title: "project 1", description: "desc 1", link: ""}, { title: "project 2", description: "desc 2", link: ""}],
       currentProject: null,
       currentIndex: -1,
+      title: null,
+      editMessage: 'Edit',
       publicPath: process.env.BASE_URL,
+      showEdit: false,
       pass: "",
       showMessage: "",
       showButtons: false,
@@ -81,11 +93,27 @@ export default {
       this.currentProject = null;
       this.currentIndex = -1;
     },
+    setActiveProject(project, index){
+      this.currentProject = project;
+      this.currentIndex = index;
+    },
+    searchTitle(title) {
+      this.title = title;
+    },
+    toggleShowEdit() {
+      if (this.showEdit == true) {
+        this.showEdit = false;
+        this.editMessage = 'Edit'
+      } else {
+        this.showEdit = true;
+        this.editMessage = 'Close edit options';
+      }
+    },
     checkPass() {
       if (this.pass == 'duh') {
         this.showButtons = true;
         this.showMessage = "You got it, Boss!"
-      } else if (this.pass == 'secret password') {
+      } else if (this.pass == 'secret password' || this.pass == 'the secret password') {
         this.showButtons = false;
         this.showMessage = "Very clever. Try again.";
       } else {
@@ -99,14 +127,34 @@ export default {
 
 <style>
   .project-card{
-
+    padding:10px;
+    margin: 10px 0px;
+    background-color:rgba(255,255,255,.7);
+    list-style-type:none;
+    border: 1px solid black;
   }
 
   .proj-title{
-
+    font-size:18pt;
   }
 
   .proj-desc{
+    font-size:12pt;
+  }
 
+  .search{
+    display:block;
+    text-align:right;
+    margin: 10px 0px;
+  }
+
+  .refreshButton{
+    float:left;
+  }
+
+  .editBox{
+    width:50%;
+    margin-left:50%;
+    border:1px dotted black;
   }
 </style>
