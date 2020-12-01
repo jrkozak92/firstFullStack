df8ac84d22f5ffe3e3a9fd757db7d7308c53b8df
jrkozak <jrkozak92@gmail.com>
2020-12-01 15:01:21 -0600

    redeploy

--- dist/js/chunk-2d0bff10.70dc3b96.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bff10"],{4053:function(n,t,a){"use strict";a.r(t);var e=function(){var n=this,t=n.$createElement,a=n._self._c||t;return a("div",{staticClass:"component"},[n._v(" Database interaction will go here ")])},c=[],i={name:"database",data:function(){return{publicPath:"/site-manager/"}}},s=i,l=a("2877"),r=Object(l["a"])(s,e,c,!1,null,null,null);t["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0bff10.70dc3b96.js.map
